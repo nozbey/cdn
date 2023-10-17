28ba055969f978784f67c6e7963921a128ab66f7
nuri <nuri.ozbey@arcelik.com>
2023-10-17 15:42:46 +0300

    tempjs

---
+++ dist/001.js
@@ -0,0 +1,8 @@
+var gridView = document.querySelector('.grid-view');
+var graphView = document.querySelector('.graph');
+var controlView = document.querySelector('.control');
+var statusType = document.querySelector('.status-type');
+
+var sensorBoxes = document.querySelector('.sensor-boxes');
+var graphArea = document.querySelector('.graph-area');
+var controlArea = document.querySelector('.control-area');
